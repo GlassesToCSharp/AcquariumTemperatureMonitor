44fd597ab9ebfbc8e39614c46a80f455a9558e3f
Thomas Bartleet <42772107+ThomasBartleet@users.noreply.github.com>
2020-06-04 11:21:51 +0100

    Implemented timer interrupt to take readings and update display.
    With major help from https://www.instructables.com/id/Arduino-Timer-Interrupts/

--- AquariumTemperatureMonitor/AquariumTemperatureMonitor.ino
+++ AquariumTemperatureMonitor/AquariumTemperatureMonitor.ino
@@ -27,16 +27,22 @@ const uint8_t oledReset = 9; // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(screenWidth, screenHeight, &Wire, oledReset);
 const uint8_t bufferLength = 50;
 char textBuffer[bufferLength];
+float temperature1 = 0.0;
+float temperature2 = 0.0;
+volatile float currentTemperature1 = 0.0;
+volatile float currentTemperature2 = 0.0;
 
 // Function declarations
 void setupScreen();
 void writeToScreen(const char* text, const uint8_t x = 0, const uint8_t y = 0);
 void displaySensorData(const float sensorData, const uint8_t x = 0, const uint8_t y = 0);
+void setupInterrupt();
 
-void setup(void) {  
+void setup(void) {
+  // Setup the OLED display screen.
   setupScreen();
-  
-  // Start up the library
+
+  // Start up the sensors.
   sensors.begin();
 
   // Display the total number of devices and temperature sensors connected.
@@ -50,7 +56,7 @@ void setup(void) {
   delay(2000);
 
   // For each device, get its address.
-  for(uint8_t i = 0; i < addressLength; i++) {
+  for (uint8_t i = 0; i < addressLength; i++) {
     sensors.getAddress(addresses[i], i);
   }
 
@@ -60,28 +66,17 @@ void setup(void) {
   sprintf(textBuffer, "Temp1:\nTemp2:");
   writeToScreen(textBuffer);
   display.display();
+
+  setupInterrupt();
 }
 
 void loop(void) {
-  uint32_t startTime = millis();
-  // Send the command to all sensors on the bus to get temperature readings.
-  sensors.requestTemperatures(); 
-  
-  // Once complete, get the actual temperatures in Celsius.
-  float temperature1 = sensors.getTempC(addresses[0]);
-  float temperature2 = sensors.getTempC(addresses[1]);
-
-  // Present on the OLED display.
-  displaySensorData(temperature1, 42, 0);
-  displaySensorData(temperature2, 42, 8);
-  
-  while(millis() - startTime < updateFrequency);
 }
 
 void setupScreen() {
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     pinMode(13, OUTPUT);
-    for(;;) {
+    for (;;) {
       // Don't proceed, loop forever
       digitalWrite(13, HIGH);
       delay(500);
@@ -96,7 +91,7 @@ void setupScreen() {
   display.drawPixel(10, 10, WHITE);
   display.display();
   delay(1000); // Pause for 2 seconds
-  
+
   // Clear the buffer
   display.clearDisplay();
 }
@@ -104,10 +99,10 @@ void setupScreen() {
 void displaySensorData(const float sensorData, const uint8_t x, const uint8_t y) {
   memset(textBuffer, bufferLength, 0);
   if ((int)sensorData != DEVICE_DISCONNECTED_C) {
-    sprintf(textBuffer, 
-      "%d.%02u C  ", // Trailing space to overwrite any text left over from a disconnected sensor
-      (int)sensorData,
-      (int)((long)(sensorData * 100) - (long)sensorData * 100));
+    sprintf(textBuffer,
+            "%d.%02u C  ", // Trailing space to overwrite any text left over from a disconnected sensor
+            (int)sensorData,
+            (int)((long)(sensorData * 100) - (long)sensorData * 100));
   } else {
     sprintf(textBuffer, "No sensor");
   }
@@ -122,11 +117,74 @@ void writeToScreen(const char* text, const uint8_t x, const uint8_t y) {
   display.setTextSize(textSize);      // 1 = Normal 1:1 pixel scale
   display.setTextColor(WHITE, BLACK);        // Draw white text on black background
   display.setCursor(x, y);
-  for(uint8_t i = 0; i < bufferLength; i++) {
+  for (uint8_t i = 0; i < bufferLength; i++) {
     if (text[i] == '\0') {
       break;
     }
-    
+
     display.print(text[i]);
+  }
+}
+
+void setupInterrupt() {
+  cli(); // Stop any other interrupts.
+
+  // We want an interrupt frequency of 1Hz (1 second). The Nano has 3 timers:
+  // - Timer 0 (8-bit)
+  // - Timer 1 (16-bit)
+  // - Timer 2 (8-bit)
+  // The correct timer to use is determined by the Compare Match Register (OCRxA - x
+  // is timer number). The CMR value is determined with the following equation:
+  // CMR = [ ClockFrequency / (Prescaler * DesiredInterruptFrequency) ] - 1
+  // The clock runs at 16MHz, and the largest prescaler the interrupts can take is
+  // 1024. The equation then becomes:
+  // CMR = [ 16,000,000 / (1024 * 1) ] - 1
+  // CMR = 15,624
+  // Since 15,624 exceeds the 8-bit range of timers 0 and 2, but is within the range
+  // of timer 1, Timer 1 is the correct timer to use.
+
+  // Set up Timer 1 for 1Hz interrupt.
+
+  // Reset Timer 1 registers
+  TCCR1A = 0; // Set Timer/Counter Control Register A to Normal Operation throughout.
+  TCCR1B = 0; // Clear the Timer/Counter Control Register B.
+  TCNT1  = 0; // Clear the Timer/Counter (initialise to 0).
+
+  // Set the Compare Match Register A for 1Hz triggers.
+  OCR1A = 15624; // From equation above.
+
+  // Turn on Clear Timer on Compare Match (CTC) mode in the Timer/Counter Control
+  // Register A.
+  TCCR1B |= (1 << WGM12); // Raise the WGM12 bit.
+
+  // Set the Clock Select bits (CS12:10) to the 1024 prescaler value.
+  TCCR1B |= ((1 << CS12) | (1 << CS10));
+
+  // Enable the timer interrupt in the Timer/Counter Interrupt Mask Register.
+  TIMSK1 |= (1 << OCIE1A); // Enable the timer on the CMR for Register A.
+
+  sei(); // Allow interrupts
+}
+
+// Interrupt Service Routine for Timer 1.
+// Timer 1 Compare Match A.
+// On Compare Match A == CMR A.
+ISR(TIMER1_COMPA_vect) {
+  // Send the command to all sensors on the bus to get temperature readings.
+  sensors.requestTemperatures();
+
+  // Once complete, get the actual temperatures in Celsius.
+  temperature1 = sensors.getTempC(addresses[0]);
+  temperature2 = sensors.getTempC(addresses[1]);
+
+  // Present readings on the OLED display.
+  if (currentTemperature1 != temperature1) {
+    currentTemperature1 = temperature1;
+    displaySensorData(temperature1, 42, 0);
+  }
+
+  if (currentTemperature2 != temperature2) {
+    currentTemperature2 = temperature2;
+    displaySensorData(temperature2, 42, 8);
   }
 }
