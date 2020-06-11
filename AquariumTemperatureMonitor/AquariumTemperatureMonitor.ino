637b5e59462e4f4357ae69ea04ea79ee8bf767ec
Thomas Bartleet <42772107+ThomasBartleet@users.noreply.github.com>
2020-06-11 12:41:17 +0100

    Added external interrupt functionality (with debounce).

--- AquariumTemperatureMonitor/AquariumTemperatureMonitor.ino
+++ AquariumTemperatureMonitor/AquariumTemperatureMonitor.ino
@@ -7,6 +7,7 @@
 #include <Wire.h>
 
 const uint16_t updateFrequency = 1000; // Update every 1s (1000ms).
+const uint8_t debounceDelay = 50; // Debounce delay in ms.
 
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices
@@ -26,6 +27,7 @@ const uint8_t screenWidth = 128; // OLED display width, in pixels
 const uint8_t screenHeight = 32; // OLED display height, in pixels
 const uint8_t oledReset = 9; // Reset pin # (or -1 if sharing Arduino reset pin). Use LED_BUILTIN for NodeMCU applications!!
 Adafruit_SSD1306 display(screenWidth, screenHeight, &Wire, oledReset);
+
 const uint8_t bufferLength = 50;
 char textBuffer[bufferLength];
 float temperature1 = 0.0;
@@ -37,7 +39,8 @@ volatile float currentTemperature2 = 0.0;
 void setupScreen();
 void writeToScreen(const char* text, const uint8_t x = 0, const uint8_t y = 0);
 void displaySensorData(const float sensorData, const uint8_t x = 0, const uint8_t y = 0);
-void setupInterrupt();
+void setupExternalInterrupt();
+void setupTimerInterrupts();
 
 void setup(void) {
   // Setup the OLED display screen.
@@ -68,11 +71,14 @@ void setup(void) {
   writeToScreen(textBuffer);
   display.display();
 
-  setupInterrupt();
+  cli(); // Stop any other interrupts.
+  setupExternalInterrupt();
+  setupTimerInterrupts();
+  sei(); // Enable interrupts
 }
 
 void loop(void) {
-  LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_ON);
+  LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_ON, TIMER1_ON, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_ON);
 }
 
 void setupScreen() {
@@ -129,9 +135,7 @@ void writeToScreen(const char* text, const uint8_t x, const uint8_t y) {
   }
 }
 
-void setupInterrupt() {
-  cli(); // Stop any other interrupts.
-
+void setupTimerInterrupts() {
   // We want an interrupt frequency of 1Hz (1 second). The Nano has 3 timers:
   // - Timer 0 (8-bit)
   // - Timer 1 (16-bit)
@@ -157,7 +161,7 @@ void setupInterrupt() {
   OCR1A = 15624; // From equation above.
 
   // Turn on Clear Timer on Compare Match (CTC) mode in the Timer/Counter Control
-  // Register A.
+  // Register B.
   TCCR1B |= (1 << WGM12); // Raise the WGM12 bit.
 
   // Set the Clock Select bits (CS12:10) to the 1024 prescaler value.
@@ -166,7 +170,27 @@ void setupInterrupt() {
   // Enable the timer interrupt in the Timer/Counter Interrupt Mask Register.
   TIMSK1 |= (1 << OCIE1A); // Enable the timer on the CMR for Register A.
 
-  sei(); // Allow interrupts
+
+  //------------------
+  // Set up Timer 2 for 1kHz interrupt.
+
+  // Reset Timer 1 registers
+  TCCR2A = 0; // Set Timer/Counter Control Register A to Normal Operation throughout.
+  TCCR2B = 0; // Clear the Timer/Counter Control Register B.
+  TCNT2  = 0; // Clear the Timer/Counter (initialise to 0).
+
+  // Set the Compare Match Register A for 1kHz triggers.
+  OCR2A = 124; // From equation above.
+
+  // Turn on Clear Timer on Compare Match (CTC) mode in the Timer/Counter Control
+  // Register A.
+  TCCR2A |= (1 << WGM21); // Raise the WGM21 bit.
+
+  // Set the Clock Select bits (CS22:20) to the 128 prescaler value.
+  TCCR2B |= ((1 << CS22) | (1 << CS20));
+
+  // Enable the timer interrupt in the Timer/Counter Interrupt Mask Register.
+  //  TIMSK2 |= (1 << OCIE2A); // Enable the timer on the CMR for Register A.
 }
 
 // Interrupt Service Routine for Timer 1.
@@ -190,4 +214,65 @@ ISR(TIMER1_COMPA_vect) {
     currentTemperature2 = temperature2;
     displaySensorData(temperature2, 42, 8);
   }
+}
+
+// Interrupt Service Routine for Timer 2.
+// Timer 2 Compare Match A.
+// On Compare Match A == CMR A.
+ISR(TIMER2_COMPA_vect) {
+  static uint16_t counter = 0;
+  counter++;
+
+  if (counter > debounceDelay) {
+    counter = 0;
+    TIMSK2 &= ~(1 << OCIE2A); // Disable the debounce timer Timer2. No need to keep it running.
+    EIMSK |= (1 << INT0); // Enable the external interrupt INT0.
+  }
+}
+
+void setupExternalInterrupt() {
+  // We want an external interrupt occurring so that we can change the display. The
+  // Nano (ATMega328P) has multiple external interrupt sources:
+  // - External Interrupt Request 0  (pin D2)          (INT0_vect)
+  // - External Interrupt Request 1  (pin D3)          (INT1_vect)
+  // - Pin Change Interrupt Request 0 (pins D8 to D13) (PCINT0_vect)
+  // - Pin Change Interrupt Request 1 (pins A0 to A5)  (PCINT1_vect)
+  // - Pin Change Interrupt Request 2 (pins D0 to D7)  (PCINT2_vect)
+  // The external interrupts we want to look at are INT0 and INT1.
+
+  // Set up I/O pin.
+  MCUCR &= ~(1 << PUD); // Clear the PUD bit to enable internal pull-ups.
+  DDRD &= ~(1 << DDD2); // Clear the Data Direction D2 bit to set the pin to "input".
+  PORTD |= (1 << PORTD2); // Raise the PD2 bit to apply internal pull-up to PD2. Does
+  // not work if PUD (in MCUCR) is 1
+
+  // Set up External Interrupt registers.
+
+  // Reset External Interrupt Control Register A
+  EICRA = 0; // Reset the register.
+  EICRA |= (1 << ISC01); // Trigger interrupt on INT0 on a falling edge.
+  // NOTE: INT1 has been set to low-level interrupt request.
+
+  // Raise the interrupt flag. This should prevent the interrupt from being triggered
+  // on setup.
+  EIFR |= (1 << INTF0);
+
+  // Enable the external interrupt.
+  EIMSK = (1 << INT0); // Disable INT1, and enable INT0.
+
+  // Clear the interrupt flag.
+  //  EIFR &= ~(1 << INTF0);
+}
+
+// Interrupt Service Routine for external interrupt 0 (INT0).
+ISR(INT0_vect) {
+  // Disable external interrupt and enable Timer2 to accommodate the debounce.
+  // Disabling INT0 will prevent future triggers during the debounce period,
+  // and use Timer2 to determine for how long to remain disabled.
+  EIMSK &= ~(1 << INT0); // Disable the external interrupt INT0.
+  TIMSK2 |= (1 << OCIE2A); // Enable the debounce timer Timer2.
+
+  const uint8_t ledPin = 13;
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, !digitalRead(ledPin));
 }
